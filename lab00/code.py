638a90a2c32c7d8e919251f3f111d7af612aa021
Rui Lv <3602386563@qq.com>
2023-09-09 21:54:35 +0800

    ok

--- lab00/code.py
+++ lab00/code.py
@@ -1,3 +1,4 @@
+
 def get_airspeed_velocity_of(unladen_swallow):
   if unladen_swallow.type == "african":
     return # redacted
@@ -9,8 +10,9 @@ def fizzbuzz(num):
     print(f"{num}: fizzbuzz")
   elif str(num) in ["3", "6", "9", "12", "15", "18"]:
     print(f"{num}: fizz")
-  elif str(num) in ["5", "10", "15"]:
+  elif str(num) in ["5", "10", "15","19"]:
     print(f"{num}: buzz")
 
 for i in range(1, 20):
   fizzbuzz(i)
+
